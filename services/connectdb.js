46a96223092a2209976949ad7fd3c04be6aaa9b5
steliosaz <steliosazis@gmail.com>
2023-07-16 00:03:00 +0300

    message

--- services/connectdb.js
+++ services/connectdb.js
@@ -12,6 +12,6 @@ db.connect(function(err) {
     if (err) {
         throw err
     }
-    console.log("remote database connected");
+    console.log("database connected");
 });
 module.exports = db;
